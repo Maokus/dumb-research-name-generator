71e1518eb6b2df230aba03a14325ef0d5c0100f8
Markus Ang <maokooss@gmail.com>
2025-12-17 22:24:20 +0800

    fix build, add opengraph

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base so the generated index.html references assets relative to the
+  // published path on maok.us: https://maok.us/playbox/projects/rng/
+  base: '/playbox/projects/rng/',
   plugins: [react()],
 })
